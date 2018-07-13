4587f814d34fb7cc1c90a12863331a56fee35b3e
LunaPC <lunad3@uci.edu>
2018-07-12 20:48:56 -0700

    generate random Blocks, havent ensured no repeats yet

--- clickgame/src/App.js
+++ clickgame/src/App.js
@@ -4,19 +4,19 @@ import Title from "./components/Title";
 import Block from "./components/Block";
 import "./App.css";
 
-
-
 class App extends Component {
 
   COLORS = ["yellow","green","red","blue"];
   COLORSUSED = 0||this.COLORS.length;
   COLORSPERBLOCK = 2;
   NUMBLOCKS = 4;
+  colorId = this.NUMBLOCKS + 1;
 
   choicesTree = (node,counter)=>{
     node.children = {};
     node.allChildrenSelected = false;
     node.isSelcted = false;
+    node.key = this.colorId++;
     if (counter>0){
       counter--;
       node.isLeaf = false;
@@ -37,7 +37,8 @@ class App extends Component {
     choices: this.choicesTree({},this.COLORSPERBLOCK),
     choicesLeft: this.optionsLeft(0)
   };
-//---------------------------------------------------------------------------------
+
+  //---------------------------------------------------------------------------------
 
   // returns 0 if all children have been selected, else a child is not selected
   updateChildren = (node)=>{
@@ -68,46 +69,47 @@ class App extends Component {
     return choices;
   };
 
-  refineChoices = (colorArr,blocksArr)=>{
-    let choices = this.COLORS;
-    const colorIndex = colorArr.length-1;
-    if(colorIndex === -1){
-      return choices;
-    }
-    const color = colorArr[colorIndex];
-    let newBlocksArr = [];
-    for(let blockIndex=0; blockIndex<blocksArr.length; blockIndex++){
-      if(blocksArr[blockIndex][colorIndex] === color){
-        newBlocksArr.push(blocksArr[blockIndex]);
-      }
-    }
-
-    return choices;
-  };
-
   randomIndex = (arr)=>{
-    const index = arr[Math.floor(Math.random() * arr.length)];
-    return index;
+    return Math.floor(Math.random() * arr.length);
   };
 
-  newColorArray = (colorArr,blocksArr)=>{
-
+  randomColorArr = (result,node,count)=>{
+    if(count > 0){
+      count--;
+      const selectedColor = this.COLORS[this.randomIndex(this.COLORS)];
+      const colorObj ={
+        color:selectedColor,
+        key:node.children[selectedColor].key
+      }
+      result.push(colorObj);
+      return this.randomColorArr(result,node.children[selectedColor],count);
+    }else{
+      return result;
+    }
   };
-
-  randomColorArray = (colorArr,blocksArr)=>{
-    
-  }
 
   newBlock = ()=>{
-    const newBlock = <Block colorArr={this.newColorArray([],this.state.selected)}/>
-    return newBlock;
+    const block = <Block colorArr={this.randomColorArray([],this.state.selected)}/>
+    return block;
   };
 
   generateBlocks = ()=>{
     let blocks = [];
-    
+    const uniqueBlockIndex = null;//Math.floor(Math.random() *this.NUMBLOCKS);
+    console.log(uniqueBlockIndex);
+    for (let i=0; i<this.NUMBLOCKS; i++){
+      console.log("B: "+(i+1) + "/"+this.NUMBLOCKS);
+      let colorDataObj = null;
+      if (i === uniqueBlockIndex){
+        // colorDataObj = this.newColorArray();
+      }else{
+        colorDataObj = this.randomColorArr([],this.state.choices,this.COLORSPERBLOCK);
+      }
+      console.log(colorDataObj);
+      blocks.push(<Block colorArr={colorDataObj} key={i}/>);
+    }
+    return blocks;
   };
-
 
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
--- clickgame/src/components/Block/Block.js
+++ clickgame/src/components/Block/Block.js
@@ -3,8 +3,8 @@ import "./Block.css";
 
 const Block = props=>(
     <div className="block">
-        {props.colorArr.map(color=>{
-            return <div className="colorRow" style={{backgroundColor:color}} key={color}></div>
+        {props.colorArr.map(colorObj=>{
+            return <div className="colorRow" style={{backgroundColor:colorObj.color}} key={colorObj.key}></div>
         })}
     </div>
 );
